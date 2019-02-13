443dd02560655650801814838aff14d5829ae2d7
Ole Kristian Losvik <losolio@github.user>
2019-02-14 00:19:14 +0100

    renamed

--- includes/class-eventi-options.php
+++ includes/class-eventi-options.php
@@ -2,21 +2,21 @@
 
 class Eventi_Options {
 	function __construct() {
-		add_action( 'admin_menu', array( $this, 'eventi_register_options_page' ) );
+		add_action( 'admin_menu', array( $this, 'create_eventi_settings_page' ) );
 	}
 
-	function eventi_register_options_page() {
+	function create_eventi_settings_page() {
 		$parent_slug = 'edit.php?post_type=eventi_event';
 		$page_title  = 'Eventi configuration';
 		$menu_title  = 'Configuration';
 		$capability  = 'manage_options';
 		$menu_slug   = 'eventiconfig';
-		$function    = array( $this, 'eventi_options_page' );
+		$function    = array( $this, 'eventi_settings_page_content' );
 
 		add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
 	}
 
-	function eventi_options_page() {
+	function eventi_settings_page_content() {
 		echo 'This is the page content';
 	}
 }
