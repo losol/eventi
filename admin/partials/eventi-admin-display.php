4a0be87e5add668bfd4f8889bac154db57d5a6a0
Ole Kristian Losvik <losolio@github.user>
2019-02-13 16:25:39 +0100

    Added wppb boilerplate

---
+++ admin/partials/eventi-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://losol.no
+ * @since      1.0.0
+ *
+ * @package    Eventi
+ * @subpackage Eventi/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-eventi-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://losol.no
+ * @since      1.0.0
+ *
+ * @package    Eventi
+ * @subpackage Eventi/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Eventi
+ * @subpackage Eventi/includes
+ * @author     Ole Kristian Losvik <losvik@gmail.com>
+ */
+class Eventi_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
