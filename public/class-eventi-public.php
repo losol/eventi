f7c62fc89bbf5df3b3c9fc5ebf976a257d46cb67
Ole Kristian Losvik <losolio@github.user>
2019-02-16 20:52:07 +0100

    Added JSON-LD

--- public/class-eventi-public.php
+++ public/class-eventi-public.php
@@ -13,6 +13,9 @@ class Eventi_Public {
 		add_filter( 'single_template', array( $this, 'load_event_template' ) );
 		add_filter( 'archive_template', array( $this, 'load_event_archive_template' ) );
 
+		// Add JSON-LD metadata
+		add_action( 'wp_head', array( $this, 'insert_json_ld' ) );
+
 	}
 
 	function enable_api() {
@@ -62,85 +65,43 @@ class Eventi_Public {
 		return $template;
 	}
 
+	function insert_json_ld() {
+		if ( is_singular( 'eventi_event' ) ) {
+			the_post();
+			$context          = 'https://schema.org';
+			$type             = 'Event';
+			$name             = get_the_title();
+			$start_date       = '2018-10-30';
+			$description      = get_the_excerpt();
+			$place            = 'Kult sted';
+			$location_city    = 'Kul by';
+			$location_country = 'Kult land';
 
-
-
-	function eventi_events_full( $atts ) {
-		// - define arguments -
-		extract(
-			shortcode_atts(
-				array(
-					'limit' => '10', // # of events to show
+			$metadata_array = array(
+				'@context'    => $context,
+				'@type'       => $type,
+				'name'        => $name,
+				'description' => $description,
+				'startDate'   => $start_date,
+				'location'    => array(
+					'@type' => 'Place',
+					'name'  => $place,
+					'address' => array(
+						'@type' => 'PostalAddress',
+						'addressLocality' => $location_city,
+						'addressCountry'  => $location_country,
+					),
 				),
-				$atts
-			)
-		);
+			);
 
-		// ===== OUTPUT FUNCTION =====
-		ob_start();
+			$metadata_json  = json_encode( $metadata_array, JSON_UNESCAPED_SLASHES );
 
-		$today6am = strtotime( 'today 6:00' ) + ( get_option( 'gmt_offset' ) * 3600 );
-		// - query -
-		global $wpdb;
-		$querystr = "
-		SELECT *
-		FROM $wpdb->posts wposts, $wpdb->postmeta metastart, $wpdb->postmeta metaend
-		WHERE (wposts.ID = metastart.post_id AND wposts.ID = metaend.post_id)
-		AND (metaend.meta_key = 'eventi_enddate' AND metaend.meta_value > $today6am )
-		AND metastart.meta_key = 'eventi_enddate'
-		AND wposts.post_type = 'eventi_event'
-		AND wposts.post_status = 'publish'
-		ORDER BY metastart.meta_value ASC LIMIT $limit
-	 ";
-		$events   = $wpdb->get_results( $querystr, OBJECT );
-		// - declare fresh day -
-		$daycheck = null;
-		// - loop -
-		if ( $events ) :
-			global $post;
-			foreach ( $events as $post ) :
-				setup_postdata( $post );
-				// - custom variables -
-				$custom = get_post_custom( get_the_ID() );
-				$sd     = $custom['eventi_startdate'][0];
-				$ed     = $custom['eventi_enddate'][0];
-				// - determine if it's a new day -
-				$longdate = date( 'l, F j, Y', $sd );
-				if ( $daycheck == null ) {
-					echo '<h2 class="full-events">' . $longdate . '</h2>'; }
-				if ( $daycheck != $longdate && $daycheck != null ) {
-					echo '<h2 class="full-events">' . $longdate . '</h2>'; }
-				// - local time format -
-				$time_format = get_option( 'time_format' );
-				$stime       = date( $time_format, $sd );
-				$etime       = date( $time_format, $ed );
-				// - output - ?>
-	<div class="full-events">
-		<div class="text">
-			<div class="title">
-				<div class="time"><?php echo $stime . ' - ' . $etime; ?></div>
-				<div class="eventtext"><?php the_title(); ?></div>
-			</div>
-		</div>
-		<div class="desc">
-				<?php
-				if ( strlen( $post->post_content ) > 150 ) {
-					echo substr( $post->post_content, 0, 150 ) . '...';
-				} else {
-						echo $post->post_content; }
-				?>
-			</div>
-	</div>
-				<?php
-				// - fill daycheck with the current day -
-				$daycheck = $longdate;
-		endforeach;
-		else :
-		endif;
-		// ===== RETURN: FULL EVENTS SECTION =====
-		$output = ob_get_contents();
-		ob_end_clean();
-		return $output;
+			$head_script = "
+				<script type=\"application/ld+json\">
+					$metadata_json
+				</script>";
+			echo $head_script;
+		}
 	}
-
 }
+
