163b727475958d8056c1cc3c789b33a96a3858bb
Ole Kristian Losvik <losolio@github.user>
2019-02-17 17:47:10 +0100

    Tried to add sorting by meta field

--- public/templates/archive-eventi.php
+++ public/templates/archive-eventi.php
@@ -13,7 +13,7 @@ get_header();
 
 		<?php
 		if ( have_posts() ) :
-		?>
+			?>
 
 			<header class="page-header">
 				<?php
@@ -26,6 +26,9 @@ get_header();
 			$args = [
 				'post_type'      => 'eventi',
 				'posts_per_page' => 100,
+				'meta_key'       => 'eventi_startdate',
+				'orderby'        => 'meta_value',
+				'order'          => 'ASC',
 			];
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) {
@@ -35,7 +38,7 @@ get_header();
 				<?php
 			}
 
-		// If no content, include the "No posts found" template.
+			// If no content, include the "No posts found" template.
 		else :
 			require plugin_dir_path( dirname( __FILE__ ) ) . 'templates/template-parts/event-none.php';
 		endif;
